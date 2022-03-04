85ca0300e030ec2be8db8df5d43b3555e875bfe8
MiraRose <miraastar@gmail.com>
2022-03-04 13:49:24 -0500

    Got a simple run command working in app

--- src/main/java/SweaterWeather/App.java
+++ src/main/java/SweaterWeather/App.java
@@ -1,8 +1,33 @@
 package SweaterWeather;
 
+import SweaterWeather.Model.Configuration;
+import SweaterWeather.Model.SimpleWeather;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+
+
+
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+
+        WeatherApi weatherApi = new WeatherApi();
+        ConfigReader configReader = new ConfigReader();
+        SimpleWeatherBuilder simpleWeatherBuilder = new SimpleWeatherBuilder();
+        OutfitSuggester outfitSuggester = new OutfitSuggester();
+
+        JsonObject weather = weatherApi.getWeatherJsonByCityState(args[1], args[2]);
+        SimpleWeather simpleWeather = simpleWeatherBuilder.build(weather);
+        Configuration configuration = configReader.read(args[0]);
+
+        List<String> outfits = outfitSuggester.suggestOutfitList(configuration, simpleWeather);
+        System.out.println("Suggested Clothing Items to Bring");
+        for (String outfit : outfits) {
+            System.out.println(outfit);
+        }
     }
 }
